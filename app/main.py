da6c1a7c05c68d5728518bf1d92fa3e242464d7f
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-07-13 15:10:58 +0200

    implement requested classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.hidden = False
+        self.health = 100
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: Animal) -> None:
+        if (
+            isinstance(target, Herbivore)
+            and not target.hidden
+        ):
+            target.health -= 50
+            if target.health <= 0:
+                Animal.alive.remove(target)
